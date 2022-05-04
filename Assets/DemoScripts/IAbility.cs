0a79a1718f83205dc47a408e3ba0560f60d0cd39
AnnaNikiforova90 <mamadownshifter@gmail.com>
2022-05-04 23:41:34 +0500

    HomeWork3

--- Assets/DemoScripts/IAbility.cs
+++
@@ -1,8 +0,0 @@
-﻿using Battle.Actors;
-namespace Battle.Abilities
-{
-    public interface IAbility
-    {
-        void Use(Actor target);
-    }
-}
---
+++ Assets/Textures/Treasure Hunters/The Crusty Crew/Sprites/Crabby/01-Idle/PixelCrew/LayerCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerCheck : MonoBehaviour
+{
+    [SerializeField] private LayerMask _groundlayer;
+    private Collider2D _collider;
+    public bool IsTouchingLayer;
+
+    private void Awake()
+    {
+        _collider = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        IsTouchingLayer = _collider.IsTouchingLayers(_groundlayer);
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        IsTouchingLayer = _collider.IsTouchingLayers(_groundlayer);
+    }
+}
+
